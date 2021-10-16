181c33fb4b12f15086c93b0a834d8e286308373c
Jamoliddin <jm@gamil.com>
2021-10-16 14:56:55 +0300

    dev change

--- main.py
+++ main.py
@@ -528,6 +528,9 @@
 
 # povtoreniye esli grupa 
 
+file=open('test.txt', 'r')
+result=file.read()
+print(len(result))
 
-
-
+with open('result.txt', 'w') as f:
+    f.write(f"test.txt: {len(result)}")
